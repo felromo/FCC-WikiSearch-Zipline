0918fb9f735262416c7f8520d5a58fdbbba8bfed
Tyrantz <raymanv3@gmail.com>
2016-01-19 02:22:43 +0000

    updated frontend

--- dist/includes/main.js
+++ dist/includes/main.js
@@ -1,3 +1,4 @@
+// re-write this as angular
 $(document).ready(function () {
   $(".search-initial").on("click", function () {
     $('input').addClass('search-bar-after');
@@ -5,3 +6,26 @@ $(document).ready(function () {
     $(this).addClass('search-after');
   });
 });
+
+angular.module('wikiApp', [])
+
+.factory('wikiFactory', ['$http', function ($http) {
+  var url = "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json";
+  var callback = "&callback=JSON_CALLBACK;"
+  return {
+    test: function () {
+      // returns the promise for further processing
+      return $http.jsonp(url+callback);
+    },
+  };
+}])
+
+.controller('wikiController', ['wikiFactory', function (wikiFactory) {
+
+  wikiFactory.test().then(function (reply) {
+    console.log(reply);
+  });
+
+  console.log("test");
+
+}]);
