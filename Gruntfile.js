5b7c645d1eec73307ae59bef98c8bd8984e0a85d
Tyrantz <raymanv3@gmail.com>
2016-01-19 02:41:49 +0000

    replaces express with connect and adds tasks to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,31 @@ module.exports = function (grunt) {
                 dest: 'build/<%= pkg.name %>.min.js'
             }
         },
+        connect: {
+          options: {
+            port: 9000,
+            hostname: 'localhost',
+            livereload: 35729
+          },
+          dev: {
+            options: {
+              open: true,
+              middleware: function (connect) {
+                return [
+                  connect().use('/bower_components', connect.static('./bower_components')),
+                  connect.static('./dev/public')
+                ];
+              }
+            }
+          }
+        },
+        wiredep: {
+            task: {
+                devDependencies: true,
+                src: ['./dev/public/index.html']
+            }
+        },
+        // replaced by connect task
         express: {
             dev: {
               options: {
@@ -40,17 +65,29 @@ module.exports = function (grunt) {
             }
         },
         watch: {
-            express: {
-                files: ['**/*.js', '**/*.html', '**/*.css'],
-                tasks: ['express:dev'],
-                options: {
-                    livereload: true
-                }
+            // bower: {
+            //     files: ['bower.json'],
+            //     tasks: ['wiredep']
+            // },
+            // express: {
+            //     files: ['*|)}>#*.js', '*|)}>#*.html', '*|)}>#*.css'],
+            //     tasks: ['express:dev'],
+            //     options: {
+            //         livereload: true
+            //     }
+            // },
+          livereload: {
+            options: {
+              livereload: '<%= connect.options.livereload %>'
             },
+            files: [
+              'dev/public/{,*/}*.{html,js}'
+            ]
+          },
             sass: {
                 files: ['**/*.scss'],
                 tasks: ['sass:dev']
-            }
+            },
         },
     });
 
@@ -58,7 +95,17 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-express-server');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-build-control');
-    grunt.registerTask('server', ['express:dev', 'watch']);
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-wiredep');
+
+
+    //grunt.registerTask('server', ['express:dev', 'watch']);
+    grunt.registerTask('serve', 'Starting Web Server', function () {
+      grunt.task.run([
+        // 'wiredep',
+        'connect',
+        'watch'
+      ]);
+    });
     grunt.registerTask('dist', ['express:dist', 'watch']);
 };
