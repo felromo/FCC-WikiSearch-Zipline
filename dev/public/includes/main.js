de5b2d8ad7147122bba24b1572358b85c9f1b643
Tyrantz <raymanv3@gmail.com>
2016-01-19 05:32:18 +0000

    adds basic links for api calls

--- dev/public/includes/main.js
+++ dev/public/includes/main.js
@@ -10,19 +10,39 @@ $(document).ready(function () {
 angular.module('wikiApp', [])
 
 .factory('wikiFactory', ['$http', function ($http) {
-  var url = "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json";
-  var callback = "&callback=JSON_CALLBACK;"
+  var search_term = "&srsearch=freedom";
+
+  var base_url = "https://en.wikipedia.org/w/api.php?";
+  var url = "action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json";
+  var search_list = "action=query&list=search&format=json&titles=Independence";
+  var search_titles = "action=query&prop=extracts|images|pageimages&format=json&titles=Independence";
+  var callback = "&callback=JSON_CALLBACK";
   return {
     test: function () {
       // returns the promise for further processing
-      return $http.jsonp(url+callback);
+      return $http.jsonp(base_url+url+callback);
     },
+    setSearchTerm: function (term) {
+      search_term = "&srsearch=" + term;
+    },
+    getList: function () {
+      return $http.jsonp(base_url+search_list+search_term+callback);
+    },
+    titles: function () {
+      return $http.jsonp(base_url+search_titles+callback);
+    }
   };
 }])
 
 .controller('wikiController', ['wikiFactory', function (wikiFactory) {
 
   wikiFactory.test().then(function (reply) {
+    console.log(reply);
+  });
+  wikiFactory.getList().then(function (reply) {
+    console.log(reply);
+  });
+  wikiFactory.titles().then(function (reply) {
     console.log(reply);
   });
 
