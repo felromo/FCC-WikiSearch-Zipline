05d9ac2e6ac6720fee50b4a1d841e61f601e1a39
Tyrantz <raymanv3@gmail.com>
2016-01-20 03:17:59 +0000

    updated animation and basic api calls

--- includes/main.js
+++ includes/main.js
@@ -7,25 +7,66 @@ $(document).ready(function () {
   });
 });
 
-angular.module('wikiApp', [])
+angular.module('wikiApp', ['ngSanitize'])
 
 .factory('wikiFactory', ['$http', function ($http) {
-  var url = "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json";
-  var callback = "&callback=JSON_CALLBACK;"
+  var search_term = "&srsearch=freedom";
+  var search_title = "";
+
+  var base_url = "https://en.wikipedia.org/w/api.php?";
+  var url = "action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json";
+  var search_list = "action=query&list=search&format=json&titles=Independence";
+  var search_titles = "action=query&prop=extracts|images|pageimages&format=json&titles=Independence";
+  var callback = "&callback=JSON_CALLBACK";
   return {
     test: function () {
       // returns the promise for further processing
-      return $http.jsonp(url+callback);
+      return $http.jsonp(base_url+url+callback);
     },
+    setSearchTerm: function (term) {
+      search_term = "&srsearch=" + term;
+    },
+    setTitle: function (title) {
+      search_title = title;
+    },
+    getList: function () {
+      return $http.jsonp(base_url+search_list+search_term+callback);
+    },
+    titles: function () {
+      return $http.jsonp(base_url+search_titles+callback);
+    }
   };
 }])
 
 .controller('wikiController', ['wikiFactory', function (wikiFactory) {
 
-  wikiFactory.test().then(function (reply) {
-    console.log(reply);
-  });
+  var self = this;
+  self.list_of_articles = [];
+  self.term = "Open Source";
 
-  console.log("test");
+  // wrapper function so we won't have anything hanging in the open
+  self.init = function () {
+
+  };
+
+  self.removeLanding = function () {
+    $('h1.title').addClass('remove');
+    $('form').addClass('remove');
+    $('.random-article').addClass('remove');
+  };
+
+  self.runSearch = function () {
+    wikiFactory.setSearchTerm(self.term);
+    wikiFactory.getList().then(function (reply) {
+    self.list_of_articles = reply.data.query.search;
+    console.log(self.list_of_articles);
+    self.removeLanding();
+    }, function () {
+      console.log("error retrieving the list of articles");
+    });
+  };
+
+  // run the wrapper function
+  self.init();
 
 }]);
